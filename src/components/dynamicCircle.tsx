21fd709c6b3bcbbc328eaf7437d147663ea2e308
VitorGuilherme <alfredo.neg9@gmail.com>
2024-08-29 18:23:17 -0300

    :construction: dynamically showing presentation circles

---
+++ src/components/dynamicCircle.tsx
@@ -0,0 +1,20 @@
+const dynamicCircles = [
+  {
+    img: "about-icon",
+    name: "about",
+  },
+  {
+    img: "techs-icon",
+    name: "techs",
+  },
+  {
+    img: "projects-icon",
+    name: "projects",
+  },
+  {
+    img: "contacts-icon",
+    name: "contacts",
+  },
+];
+
+export default dynamicCircles;
--- src/components/viewer/presentation/Presentation.tsx
+++ src/components/viewer/presentation/Presentation.tsx
@@ -1,45 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import { useLanguage } from "../../../context/LanguageContext";
 import style from "./Presentation.module.css";
 
+// presentation data
+import dynamicCircles from "../../../components/dynamicCircle";
+
 export default function Presentation() {
   const { language } = useLanguage(); // Obtendo o idioma atual do contexto
+  const [animation, setAnimation] = useState(false);
+
+  const openAnimation = (screen: any) => {
+    if (screen) {
+      setAnimation(true);
+    }
+  };
 
   return (
     <div className={style.presentation_container}>
       <h2 className={style.presentation_title}>{language.title}</h2>
       <div className={style.circles_container} style={{ marginLeft: "100px" }}>
-        <div
-          id="about"
-          className={`${style.presentation_circle} ${style.first}`}
-        >
-          <img src="/presentation-icons/about_icon.png" />
-          <p>{language.about}</p>
-        </div>
-        <div
-          id="projects"
-          className={`${style.presentation_circle} ${style.second}`}
-        >
-          <img src="/presentation-icons/projects_icon.png" />
-
-          <p>{language.projects}</p>
-        </div>
-      </div>
-      <div className={style.circles_container}>
-        <div
-          id="contacts"
-          className={`${style.presentation_circle} ${style.third}`}
-        >
-          <img src="/presentation-icons/contacts_icon.png" />
-          <p>{language.contacts}</p>
-        </div>
-        <div
-          id="techs"
-          className={`${style.presentation_circle} ${style.fourth}`}
-        >
-          <img src="/presentation-icons/techs_icon.png" />
-          <p>{language.techs}</p>
-        </div>
+        {dynamicCircles.map((circle) => {
+          let name = circle.name; // about
+          let circleName = name as keyof typeof language; 
+          return (
+            <div
+              id={circle.name}
+              className={`${style.presentation_circle}`}
+            >
+              <img src={`/icons/${circle.img}.png `} />
+              <p>{language[circleName]}</p>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
